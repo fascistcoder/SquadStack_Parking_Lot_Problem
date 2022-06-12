f52d07c1f524775f34a2f10c6752a2738fb96932
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-06-12 09:20:52 +0530

    convert switch style to enhanced style which is compatible for java 17.

--- src/main/java/com/squadstack/parkinglot/util/ParkingLotCommandUtil.java
+++ src/main/java/com/squadstack/parkinglot/util/ParkingLotCommandUtil.java
@@ -5,8 +5,6 @@ import com.squadstack.parkinglot.model.Car;
 import com.squadstack.parkinglot.service.ParkingLotService;
 import lombok.AllArgsConstructor;
 
-import javax.validation.constraints.Size;
-
 /**
  * @author <a>Pulkit Aggarwal</a>
  * @version 1.0
@@ -23,34 +21,30 @@ public class ParkingLotCommandUtil {
 		String key = inputs[0];
 		int age;
 
-			switch (key) {
-			case Constants.CREATE_PARKING_LOT:
+		switch (key) {
+			case Constants.CREATE_PARKING_LOT -> {
 				int capacity = Integer.parseInt(inputs[1]);
 				parkingLotService.createParkingLot(capacity);
-				break;
-			case Constants.PARK:
+			}
+			case Constants.PARK -> {
 				age = Integer.parseInt(inputs[3]);
 				String registrationNo = inputs[1];
 				parkingLotService.park(new Car(registrationNo, age));
-				break;
-			case Constants.LEAVE:
+			}
+			case Constants.LEAVE -> {
 				int slotNumber = Integer.parseInt(inputs[1]);
 				parkingLotService.leave(slotNumber);
-				break;
-			case Constants.REG_NUMBER_FOR_CARS_WITH_AGE:
+			}
+			case Constants.REG_NUMBER_FOR_CARS_WITH_AGE -> {
 				age = Integer.parseInt(inputs[1]);
 				parkingLotService.getRegistrationNoForAge(age);
-				break;
-			case Constants.SLOTS_NUMBER_FOR_CARS_WITH_AGE:
+			}
+			case Constants.SLOTS_NUMBER_FOR_CARS_WITH_AGE -> {
 				age = Integer.parseInt(inputs[1]);
 				parkingLotService.getSlotNumbersFromAge(age);
-				break;
-			case Constants.SLOTS_NUMBER_FOR_REG_NUMBER:
-				parkingLotService.getSlotNoFromRegistrationNo(inputs[1]);
-				break;
-			default:
-				System.out.println("Input Command is Wrong");
-				break;
+			}
+			case Constants.SLOTS_NUMBER_FOR_REG_NUMBER -> parkingLotService.getSlotNoFromRegistrationNo(inputs[1]);
+			default -> System.out.println("Input Command is Wrong");
 		}
 	}
 
